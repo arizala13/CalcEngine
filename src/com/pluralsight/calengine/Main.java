1e97da9cbd055683cca94945160cf16241177c79
Andres Arizala <arizala13@gmail.com>
2020-09-29 13:25:45 -0500

    "Using switch statements"

--- src/com/pluralsight/calengine/Main.java
+++ src/com/pluralsight/calengine/Main.java
@@ -4,20 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
         double value1 = 100.0d;
-        double value2 = 50.0d;
+        double value2 = 10.0d;
         double result = 0.0d;
-        char opCode = 'a';
+        char opCode = 'd';
 
-        if(opCode == 'a')
-            result = value1 + value2;
-        else if(opCode == 's')
-            result = value1 - value2;
-        else if(opCode == 'm')
-            result = value1 * value2;
-        else if(opCode == 'd')
-            result = value1 / value2;
-        else
-            result = 0.0d;
+        switch(opCode) {
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value1 - value2;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            case 'd': {
+                if (value2 != 0)
+                    result = value1 / value2;
+                break;
+            } default: {
+                System.out.println("Invalid opCode " + opCode);
+                result = 0.0d;
+            }
+        }
 
         System.out.println(result);
     }
