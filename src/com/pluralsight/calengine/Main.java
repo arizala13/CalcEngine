0502b287ad83b70b79a44bdc4cf45f5dbd97a9b6
Andres Arizala <arizala13@gmail.com>
2020-09-29 16:28:16 -0500

    "Added interactive functionality"

--- src/com/pluralsight/calengine/Main.java
+++ src/com/pluralsight/calengine/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight.calengine;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,11 +14,36 @@ public class Main {
             for (int i = 0; i < opCodes.length; i++) {
                 results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
             }
+        } else if(args.length == 1 && args[0].equals("interactive")){
+            executeInteractively();
         } else if(args.length == 3){
             handleCommandLine(args);
         }
         else
             System.out.println("Please provide an operation code and 2 numeric values");
+
+    }
+
+    // We scan the user input and pass it to get parsed
+    static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+
+    }
+
+
+    // This allows to get the input from user and correctly categorize
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+
+        // pass to execute
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
 
     }
 
@@ -49,5 +76,26 @@ public class Main {
                 break;
         }
         return result;
+    }
+
+    static char opCodeFromString(String operationName) {
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {
+                "zero", "one", "two",
+                "three", "four", "five",
+                "six", "seven", "eight", "nine"
+        };
+        double value = 0d;
+        for(int index = 0; index < numberWords.length; index++) {
+            if(word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
     }
 }
