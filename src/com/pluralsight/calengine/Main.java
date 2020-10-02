3093e045f3801b6c186c468423bb5bc8f5f1be82
Andres Arizala <arizala13@gmail.com>
2020-10-02 15:10:33 -0500

    "Start of Working with Classes and Interfaces in Java plural sight course"

--- src/com/pluralsight/calengine/Main.java
+++ src/com/pluralsight/calengine/Main.java
@@ -6,10 +6,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
         double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
         char[] opCodes = {'d', 'a', 's', 'm'};
         double[] results = new double[opCodes.length];
+
+        // 4 references of type MathEquation
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0d,'d');
+        equations[0] = create(25.0d, 92.0d,'a');
+        equations[0] = create(225.0d, 17.0d,'s');
+        equations[0] = create(11.0d, 3.0d,'m');
 
         if(args.length == 0) {
             for (int i = 0; i < opCodes.length; i++) {
@@ -24,6 +32,16 @@ public class Main {
             System.out.println("Please provide an operation code and 2 numeric values");
 
     }
+
+    private static MathEquation create(double leftVal, double rightVal, char opCode) {
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
+    }
+
 
     // We scan the user input and pass it to get parsed
     static void executeInteractively() {
