18a8486b2bae600ba69ca4cfdcf71d2216ab66e1
Andres Arizala <arizala13@gmail.com>
2020-10-05 11:48:36 -0500

    "Updates to calc including improved encapsulation and creating default constructor. In process of moving code and modularizing. Start of Module 4."

--- src/com/pluralsight/calengine/MathEquation.java
+++ src/com/pluralsight/calengine/MathEquation.java
@@ -1,10 +1,23 @@
 package com.pluralsight.calengine;
 
 public class MathEquation {
-    double leftVal;
-    double rightVal;
-    char opCode;
-    double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    // default constructor
+    public MathEquation() {}
+
+    public MathEquation(char opCode){
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal){
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 
     public void execute() {
         switch (opCode) {
@@ -28,6 +41,30 @@ public class MathEquation {
     }
     public double getResult() {
         return result;
+    }
+
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
     }
 
 
